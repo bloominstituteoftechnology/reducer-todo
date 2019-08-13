e62a50e84574db6cad6917f974028c3bf5d63887
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-08-13 15:32:11 -0500

    step one complete

---
+++ reducer-todo-app/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    todoitem: "Let's learn reducers!",
+    completed: false,
+    id: 24601
+  };
+
+  export const titleReducer = (state, action) => {
+      switch(action.type) {
+          default: 
+            return state;
+      }
+  }
