007a624fdc45a5319da652e4c73677fd87a0cd90
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-09-10 15:39:55 -0400

    adding todos

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -1,2 +1,25 @@
 import React, { useState, useReducer } from "react";
-import { initialState, titleReducer } from "../reducers/Reducer";
+import { initialState, todoReducer } from "../reducers/Reducer";
+
+const TodoList= () => {
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+
+
+return (
+
+        <div>   
+            <button
+            onClick={() => {
+              dispatch({ type: "UPDATE_TITLE"});
+              setNewTitleText("");
+            }}
+          >
+            BTN CLICK
+           </button>
+    
+       </div>
+
+)
+
+
+};
--- my-app/src/reducers/Reducer.js
+++ my-app/src/reducers/Reducer.js
@@ -2,24 +2,35 @@
 //reducer
 export const initialState = {
     title: "Let's learn reducers!",
-    editing: false
+    completed: false
   };
 
 
 
   export const todoReducer = (state, action) => {
     switch (action.type) {
+
+        case "one":
+                return {
+                    ...state,
+                    item: 'Learn about CSS',
+                    completed: false,
+                    id: 1
+                  }
+
       case "TOGGLE_EDITING":
         return {
-            item: 'Learn about reducers',
-            completed: false,
-            id: 1
-          }
-      case "UPDATE_TITLE":
-        return {
+            ...state,
             item: 'Learn about react',
             completed: false,
             id: 2
+          }
+      case "UPDATE_TITLE":
+        return {
+            ...state,
+            item: 'Learn about Reducers',
+            completed: false,
+            id: 3
           }
       default:
         return state;
