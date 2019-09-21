9b6704fa054a6321ee199c28729e49db73140bec
AidenBow <AidenBow@users.noreply.github.com>
2019-09-20 18:56:36 -0700

    rendered list

--- list/src/App.js
+++ list/src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
 import './App.css';
+
 import {initialState, reducer} from "./reducers/reducer"
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.map(item => {
+        return <p>{item.item}</p>
+      })}
     </div>
   );
 }
--- list/src/reducers/reducer.js
+++ list/src/reducers/reducer.js
@@ -1,8 +1,19 @@
-export const initialState = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  }
+export const initialState = [{
+        item: 'aawdawdw',
+        completed: false,
+        id: 0
+    },
+    {  
+        item: 'apple',
+        completed: false,
+        id: 1
+    },
+    {  
+        item: 'pizza',
+        completed: false,
+        id: 2
+    },
+]
 
 export function reducer(state, action) {
     switch (action.type) {
