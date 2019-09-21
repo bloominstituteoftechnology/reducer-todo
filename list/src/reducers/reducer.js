5687f5e69af7e3ade4e05d6d0a2e6b78326a9b30
AidenBow <AidenBow@users.noreply.github.com>
2019-09-20 18:39:34 -0700

    created react app and reducer.js

---
+++ list/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+
+export function reducer(state, action) {
+    switch (action.type) {
+      default:
+        return state
+    }
+  }
