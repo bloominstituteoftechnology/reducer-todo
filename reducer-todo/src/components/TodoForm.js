c43d525ed294ad6c1b95573d5e5bc1f539ae489a
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-08 17:06:59 -0400

    TodoForm

---
+++ reducer-todo/src/components/TodoForm.js
@@ -0,0 +1,42 @@
+import React, { useState, useReducer } from 'react';
+import { reducer, todo } from '../reducers/todoReducer';
+
+const TodoForm = ({ dispatch }) => {
+    const [item, setItem] = useState('');
+    console.log('state.todo', state, todo);
+
+    const handleChanges = e => {
+        setItem(e.target.value);
+    };
+
+    const submitForm = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'ADD_TODO',
+            payload: item
+        })
+        setItem('');
+    }
+
+    const clearCompleted = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'CLEAR_COMPLETED'
+        });
+    };
+
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <label htmlFor="todo" hidden>
+                    Todo
+                </label>
+                <input name="todo" onChange={handleChanges} value={item} />
+                <button>Add Todo</button>
+                <button onClick={clearCompleted}>Clear Clear Completed</button>
+            </form>
+        </div>
+    );
+};
+
+export default TodoForm;
