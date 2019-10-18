66db34dd8b961cb4f9f56206302327ef58ef8fa0
spettigrew <sara.pettigrew5@gmail.com>
2019-10-17 20:25:47 -0600

    dependencies installed

---
+++ reducer-todo/src/reducers/Reducers.js
@@ -0,0 +1,18 @@
+
+import React, { useReducer } from 'react';
+
+ export function Reducer() {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+     {
+         item: 'Learn about reducers',
+             completed: false,
+                 id: 3892987589
+     }
+
+     return (
+         <>
+
+         </>
+     )
+ }
