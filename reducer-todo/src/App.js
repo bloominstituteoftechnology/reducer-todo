d62465b6443bb9d433279a6953a9a33760061e4d
spettigrew <sara.pettigrew5@gmail.com>
2019-10-18 15:56:44 -0600

    app, todo, addTodo.js setup

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,18 @@
 import React, { useReducer } from 'react';
+import Title from './components/Todo';
+import AddTodo from './components/AddTodo';
 
 import './App.css';
+import { Reducer } from './reducers/Reducers';
 
 function App() {
   return (
     <div className="App">
-     
+      <Reducer></Reducer>
+     <Title>My To-Do List</Title>
+     <AddTodo>
+
+     </AddTodo>
     </div>
   );
 }
---
+++ reducer-todo/src/components/AddTodo.js
@@ -0,0 +1,50 @@
+import React, { useState, useReducer } from "react";
+import { initialState, reducer } from '../reducers/title';
+
+const Todo = () => {
+    const [newTodo, setNewTodo] = useState();
+   
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const handleChanges = e => {
+        setNewTodo(e.target.value);
+    };
+
+    const handleEdit = e => {
+        e.preventDefault();
+        // setEditing(!editing);
+        dispatch({ type: "TOGGLE_EDITING" })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setNewTodo("");
+        dispatch({ type: "UPDATE_TITLE", payload: newTodo || state.todo })
+        dispatch({ type: "TOGGLE_EDITING" })
+    };
+
+    return (
+        <div>
+            {state.editing ? (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="newTodo"
+                        placeholder={state.todo}
+                        value={newTodo}
+                        onChange={handleChanges}
+                    />
+                    <button type="submit">Save</button>
+                </form>
+            ) : (
+                    <div>
+                        <h1>{state.todo}</h1>
+                        <button onClick={handleEdit}>Edit</button>
+                    </div>
+                )}
+        </div>
+    );
+};
+
+export default Todo;
