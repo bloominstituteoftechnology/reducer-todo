e12b3fda5cb1e0c76817173d5099fa95af810b0b
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-05 13:30:43 -0600

    set up initial state and default return reducer

---
+++ reducers/reducer.js
@@ -0,0 +1,26 @@
+
+export const initialState = [
+    {
+      todo: 'Organize Garage',
+      completed: false,
+      id: 1528817077286
+     
+    },
+    {
+      todo: 'Do Laundry',
+      completed: false,
+      id: 1528817084350
+    },
+  ];
+
+  export const taskReducer = (state, action) =>{
+    switch (action.type) {
+      default:
+      return state;
+    }
+  };
+
+
+
+
+
