19922224ee3f426edadb92545755f8322c55f715
theolamide <ajadidan@gmail.com>
2019-11-05 20:50:04 -0500

    Initial setup

---
+++ reducer-todo/src/Components/Todo.js
@@ -0,0 +1,2 @@
+import React, {useState, useReducer} from React;
+import {initialState,reducer} from './reducers/reducer';
---
+++ reducer-todo/src/Components/reducers/reducer.js
@@ -0,0 +1,17 @@
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589 
+};
+
+
+
+export const reducer = (state,action) => {
+    switch(action.type){
+
+
+        default:
+            return state;
+    }
+}
