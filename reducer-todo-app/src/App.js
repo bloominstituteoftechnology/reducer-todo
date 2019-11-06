008ce29114ba77db12f7b8c9fa117bbad985b7da
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-05 20:49:34 -0500

    clearing tasks not working so well?

--- reducer-todo-app/src/App.js
+++ reducer-todo-app/src/App.js
@@ -1,34 +1,32 @@
-import React, { useReducer } from 'react';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 // Reducer
 import { reducer, initialState } from "./Reducers/reducer";
 
 // Components
 import TodoForm from "./Components/TodoForm";
-import TodoList from './Components/TodoList';
-
-
+import TodoList from "./Components/TodoList";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const addTodo = todoName => {
-    dispatch({ type: "ADD_TODO", payload: todoName })
-  }
+    dispatch({ type: "ADD_TODO", payload: todoName });
+  };
 
   const toggleTodo = id => {
-    dispatch({ type: "TOGGLE_TODO", payload: id});
-  }
+    dispatch({ type: "TOGGLE_TODO", payload: id });
+  };
 
   const clearCompleted = id => {
-    dispatch({ type: "CLEAR_COMPLETED" })
-  }
-  
+    dispatch({ type: "CLEAR_COMPLETED" });
+  };
+
   return (
     <div className="App">
       <h1>Welcome to My React To-Do App!</h1>
-      <TodoForm addTodo={addTodo} clearCompleted={clearCompleted}/>
-      <TodoList {...state} toggleTodo={toggleTodo}/>
+      <TodoForm addTodo={addTodo} clearCompleted={clearCompleted} />
+      <TodoList {...state} toggleTodo={toggleTodo} />
     </div>
   );
 }
--- reducer-todo-app/src/Components/TodoForm.js
+++ reducer-todo-app/src/Components/TodoForm.js
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
-import "../../src/App.css"
+import "../../src/App.css";
 
 const TodoForm = ({ addTodo, clearCompleted }) => {
-    const [todoInput, setTodo] = useState('');
+  const [todoInput, setTodo] = useState("");
 
+  const handleChanges = e => {
+    setTodo(e.target.value);
+  };
 
-    const handleChanges = (e) => {
-        setTodo(e.target.value);
-    };
-       
-const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     addTodo(todoInput);
-}
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={todoInput}
-                name="task"
-                onChange={handleChanges}
-            />
-            <button type="submit">Add Task</button>
-            <button className="clear-completed" onClick={clearCompleted}>Delete Task</button>
-        </form>
-    )
-}
-export default TodoForm
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={todoInput}
+        name="task"
+        onChange={handleChanges}
+      />
+      <button type="submit">Add Task</button>
+      <button className="clear-completed" onClick={clearCompleted}>
+        Delete Task
+      </button>
+    </form>
+  );
+};
+export default TodoForm;
