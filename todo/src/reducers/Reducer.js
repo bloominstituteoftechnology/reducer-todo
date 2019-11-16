2b6279f5d0b6ca6ce9bf782e6e2ee3c7e8cce12f
Veto Ramirez <vetorobert3@gmail.com>
2019-11-16 08:42:22 -0800

    created a reducer folder and reducer component.

---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 1
+}
+
+export const todoReducer = (state, action) => {
+  switch(action.type){
+    return state ;
+  }
+}
