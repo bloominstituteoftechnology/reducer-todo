aff1751cc3e9f7af54f06db6b1d4d226a8a283b9
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-11-18 13:32:42 -0800

    i have an idea of what im doing now

--- client/src/components/TodoForm.js
+++ client/src/components/TodoForm.js
@@ -2,59 +2,93 @@ import React, { useState, useReducer } from "react";
 import { initialState, todoReducer } from "../reducers";
 
 export const TodoForm = () => {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [state, dispatch] = useReducer(todoReducer, { todos: [] });
   const [todo, addTodo] = useState();
 
   const handleChange = e => {
-    addTodo([e.target.value]);
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
+    addTodo(e.target.value);
   };
 
   return (
     <div>
-      <h1>Todo List</h1>
-      <input type="text" value={todo} name="todoText" onChange={handleChange} />
-      {state.item}
-      <button
-        onSubmit={handleSubmit}
-        onClick={() => {
-          dispatch({ type: "SET_TODO", payload: todo });
-          // addTodo("");
-          console.log(todo);
-          console.log(initialState);
-        }}
-      >
-        Add Todo
-      </button>
-      <button
-      // onClick={() => {
-      >
-        Clear Completed
-      </button>
-      {!state.completed ? (
-        <div
-          onClick={() => {
-            dispatch({ type: "SET_COMPLETED" });
-            console.log(state);
+      <form>
+        <h1>Todo List</h1>
+        <input
+          type="text"
+          value={todo}
+          name="todoText"
+          onChange={handleChange}
+        />
+        <button
+          onClick={e => {
+            e.preventDefault();
+            dispatch({ type: "ADD_TODO", todo });
+            console.log(state.todos, todo);
+            addTodo("");
           }}
-          style={{ cursor: "pointer" }}
         >
-          {state.item}
-        </div>
-      ) : (
-        <div
-          onClick={() => {
-            dispatch({ type: "SET_COMPLETED" });
-            console.log(state);
+          Add Todo
+        </button>
+        <button
+          onClick={(e, idx) => {
+            e.preventDefault();
+            dispatch({ type: "CLEAR_TODO", idx });
           }}
-          style={{ textDecoration: "line-through", cursor: "pointer" }}
         >
-          {state.item}
-        </div>
-      )}
+          Clear Completed
+        </button>
+        {state.todos.map((t, idx) => (
+          <div
+            key={t.todo}
+            onClick={() => {
+              dispatch({ type: "TOGGLE_COMPLETED", idx });
+            }}
+            style={{ textDecoration: t.completed ? "line-through" : "" }}
+          >
+            {t.todo}
+          </div>
+        ))}
+        {/* {state.todos.map(t, idx => (
+          <div
+            onClick={() => {
+              dispatch({ type: "TOGGLE_COMPLETED", idx });
+            }}
+            style={{ textDecoration: t.completed ? "line-through" : "" }}
+            key={t.todo}
+          >
+            {t.todo}
+          </div>
+        ))} */}
+        {/* 
+        {!state.completed ? (
+          <div
+            onClick={() => {
+              dispatch({ type: "SET_COMPLETED" });
+            }}
+          >
+            {state.todos.map(t => (
+              <div style={{ cursor: "pointer" }} key={t.todo}>
+                {t.todo}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div
+            onClick={() => {
+              dispatch({ type: "SET_COMPLETED" });
+            }}
+          >
+            {state.todos.map(t => (
+              <div
+                style={{ textDecoration: "line-through", cursor: "pointer" }}
+                key={t.todo}
+              >
+                {t.todo}
+              </div>
+            ))}
+          </div>
+        )} */}
+      </form>
     </div>
   );
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,27 +1,35 @@
-export const initialState = [
-  {
-    item: "Learn about reducers",
-    completed: false,
-    id: 348384448239
-  },
-  {
-    item: "Do Dishes",
-    completed: false,
-    id: 348384448339
-  }
-];
+// export const initialState = [
+//   {
+//     item: "Learn about reducers",
+//     completed: false,
+//     id: 348384448239
+//   },
+//   {
+//     item: "Do Dishes",
+//     completed: false,
+//     id: 348384448339
+//   }
+// ];
 
 export const todoReducer = (state, action) => {
   switch (action.type) {
-    case "SET_TODO":
-      return [
-        ...state,
-        { item: action.payload, completed: !state.completed, id: Date.now() }
-      ];
-    case "SET_COMPLETED":
-      return { ...state, completed: !state.completed };
+    case "ADD_TODO":
+      return {
+        todos: [...state.todos, { todo: action.todo, completed: false }]
+      };
+    case "TOGGLE_COMPLETED":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        )
+      };
+
     case "CLEAR_TODO":
-      return { ...state };
+      return {
+        todos: state.todos.filter(item => {
+          return item.completed === false;
+        })
+      };
     default:
       return state;
   }
