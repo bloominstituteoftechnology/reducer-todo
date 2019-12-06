5ab4602bad3d5f7f92f5547b27da073481d5d03a
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-06 15:34:13 -0800

    css, moved clr btn to todo pg

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -4,17 +4,8 @@ import React, {useState, useReducer} from 'react';
 const TodoForm = ({dispatch}) => {
     
     const [newTask, setNewTask] = useState();
-    
-
-    const handleChange = e => {
-        setNewTask(e.target.value)
-    };
-
-    const submitTask = e =>{
-        e.preventDefault();
-    }
-
-    console.log('Adding: ', newTask);
+    const handleChange = e => {setNewTask(e.target.value)};
+    const submitTask = e =>{e.preventDefault()}
     
         return(
             <form onSubmit={submitTask}>
@@ -24,12 +15,8 @@ const TodoForm = ({dispatch}) => {
                     value={newTask}
                     onChange={handleChange}
                 />
-                <div>
-                    <button onClick={()=> dispatch({type: 'ADD_TASK', payload: newTask})}>Add</button>
-
-                    <button onClick={()=> dispatch({type: 'CLEAR_COMPLETED'})}>Clear Completed</button>
-                </div>
-            </form>
+                <button onClick={()=> dispatch({type: 'ADD_TASK', payload: newTask})}>Add</button>
+            </form>  
         );
     }
 
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -3,13 +3,14 @@ import Todo from './Todo';
 
 const TodoList = ({state, dispatch}) => {
     console.log('TodoList:', state)
-
+   
     return (
         <div className='todo-list'>
             {state.tasks.map(task=>(
                 <Todo key={task.id} task={task}
                 dispatch={dispatch}/>
             ))}
+            <button onClick={()=> dispatch({type: 'CLEAR_COMPLETED'})}>Clear Completed</button>
         </div>
     )
 };
