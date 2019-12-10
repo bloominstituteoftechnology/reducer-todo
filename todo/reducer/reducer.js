5e7591a945898e37ec40b53a765937eb89298ef8
jarrodskahill <jarrodskahill@gmail.com>
2019-12-10 13:24:10 -0800

    created reducer and initial data, then exported them

---
+++ todo/reducer/reducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+    todos: [
+        {
+            todo: 'Learn about reducers',
+            completed: false,
+            key: Date.now
+          }
+    ]
+}
+
+export const todoReducer = () => {
+    return (
+        <h1></h1>
+    )
+}
