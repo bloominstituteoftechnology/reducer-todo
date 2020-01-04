b98f5ee48ff4fe10dbb5f7f959f01f5d108851c4
CLYPR Technologies <cbanks@clypr.co>
2020-01-04 16:20:18 -0500

    completed step 1

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 
+// import Todo Components
+
+// import initialState and recuder from todoReducer
+
 function App() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  const [ input, setInput ] = useState();
+
+  const handleChanges = (e) => {
+    e.preventDefault();
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
