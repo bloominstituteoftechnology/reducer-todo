0e05ed9704dcd7ca2b9a70f59a9bdcff2b34b420
Your name <a.m.verdine@gmail.com>
2020-01-07 20:56:22 -0500

    Build reducer and set initial state

---
+++ reducer/src/reducers/reducer.js
@@ -0,0 +1,23 @@
+export const initialState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+  },
+
+  {
+    item: "Build Reducer",
+    completed: false,
+    id: 3892987590
+  },
+
+  {
+    item: "Learn about useReducer",
+    completed: false,
+    id: 3892987591
+  }
+];
+
+export const reducer = (state, action) => {
+  return state;
+};
