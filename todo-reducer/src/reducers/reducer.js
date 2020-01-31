1437d0f3d46f69d7571cae518b88d080dbdfdb37
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-30 21:35:05 -0800

    Step 1 completed. Built a simple reducer and initial state.

---
+++ todo-reducer/src/reducers/reducer.js
@@ -0,0 +1,9 @@
+export const initialState = {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+          }
+
+export const todoReducer = (state, action) => {
+        return state;
+}
