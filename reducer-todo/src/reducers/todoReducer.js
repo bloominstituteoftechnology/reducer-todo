e3a476d64969c33141428a41bec7422537f2b13d
Minakshi Verma <mvedwan@gmail.com>
2020-02-17 16:43:03 -0500

    "created todoReducer"

--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -9,15 +9,17 @@ export default todoReducer = (state, action) => {
 switch(action.type){
     case 'ADD_TODO':
         return{
-
+        ...state,
+        todo: action.payload
         }
     case 'TOGGLE_TODO':
         return{
-
+        ...state,
+        completed:!state.completed
         }
-
-}
-
+    default:
+        return state
+    }
 
 }
 
