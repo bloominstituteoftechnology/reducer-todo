f85e9965fd9d40d8821f9e19fcc0688b25374312
Michelle Cupp <shellcupp@gmail.com>
2020-02-17 17:27:27 -0600

    adding descriptive comments

--- reducertodo/src/actions/index.js
+++ reducertodo/src/actions/index.js
@@ -1,3 +1,5 @@
+//Actions are payloads of information that send data from your application to your store. They are the only source of information for the store. You send them to the store using store.dispatch().
+
 let nextTodoId = 0
 export const addTodo = text => ({
   type: 'ADD_TODO',
