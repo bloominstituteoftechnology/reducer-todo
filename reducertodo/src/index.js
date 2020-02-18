86042f23e0c07f74afd74d30aa0850e68585e095
Michelle Cupp <shellcupp@gmail.com>
2020-02-17 23:26:39 -0600

    build

--- reducertodo/src/index.js
+++ reducertodo/src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
-import * as serviceWorker from './serviceWorker';
+
 
 const store = createStore(rootReducer)
 
@@ -14,7 +14,3 @@ render(
   </Provider>,
   document.getElementById('root')
 )
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
