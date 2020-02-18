15cd0c6ddf29e0e1510e669bca56babb1b9598f0
Mandi Haase <haase1020@gmail.com>
2020-02-17 20:08:39 -0500

    fill out Todo.js component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,4 +9,41 @@ const Todo = props => {
     const handleChange = e => {
         setNewTodoItem(e.target.value);
     }
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("please add", newTodoItem)
+        dispatch({ type: "ADD_TODOITEM", payload: newTodoItem });
+        setNewTodoItem('');
+    };
+
+    handleDelete = e => {
+        e.preventDefault();
+        dispatch({ type: 'CLEAR_DONE' });
+    }
+
+    const handleToggle = e =>{
+        e.preventDefault();
+        dispatch({ type: 'TOGGLE_DONE', payload: e.target.id })
+    }
+
+    return(
+        <div>
+            <form>
+                <label htmlFor='todo'> Add Item To Do: </label>
+                <input
+                id='todo'
+                name='todo'
+                onChange={handleChange}
+                value={newTodoItem}
+                />
+            </form>
+
+            <button onClick={handleSubmit}>Add Item To Do</button>
+            <button onClick={handleDelete}>Clear Items That Are Finished</button>
+            <TodoList state={state} dispatch={dispatch} handleToggle = {handleToggle} />
+
+        </div>
+    )
 }
+
+export default Todo;
