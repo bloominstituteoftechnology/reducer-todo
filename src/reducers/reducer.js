084fc252e6273d1182dd29dfd5b76ba1956b8d58
Mandi Haase <haase1020@gmail.com>
2020-02-17 20:29:51 -0500

    add to reducer.js file

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,12 +1,25 @@
 
 
-export const initialState = {
+export const initialState = [{
 item: 'Learn about reducers',
 completed: false,
 id: Date.now()
-};
+}];
 
 
 export const reducer = (state, action) => {
-    
-}
+    switch (action.type){
+        case 'ADD_TODOITEM': {
+            return [{item:action.payload, completed: false, id: Date.now()}, ...state]
+        };
+        case 'TOGGLE_DONE': {
+            return state.map(item => item.id ===Number(action.payload) ? {...item, completed: !item.completed}: item);
+        };
+        case 'CLEAR_DONE': {
+            return state.filter(item => !item.completed);
+        };
+        default: {
+            return state;
+       }
+     }
+    }
