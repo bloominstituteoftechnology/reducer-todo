7ea97d91e273e35b5f2d26ecdd174a6994a71e2a
Jarvis Black <usnblack@gmail.com>
2020-02-28 09:03:07 -0500

    Step 1 Done

---
+++ my-app/src/Reducers/reducer.js
@@ -0,0 +1,24 @@
+export const initialState = {
+  items: [
+    { todo: "Take out Trash", completed: false, id: 1 },
+    { todo: "Wash Dishes", completed: false, id: 2 },
+    { todo: "Meal Prep", completed: false, id: 3 },
+    { todo: "Learn more Programming", completed: false, id: 4 },
+    { todo: "Hit the Gym", completed: false, id: 5 }
+  ]
+};
+
+export const reducer = (state = initialState, action) => {
+  console.log("Reducer", state, action);
+  switch (action.type) {
+    case "ADD_ITEM":
+      const newItem = { todo: action.payload };
+      return {
+        ...state,
+        items: [...state.items, newItem]
+      };
+
+    default:
+      return state;
+  }
+};
