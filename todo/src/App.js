e4b265cb6a4721baf3b2bb258eaceb15a84fce04
AhmedBarre10 <barreahmed600@gmail.com>
2020-03-16 22:34:24 -0400

    Mvp

---
+++ todo/src/App.js
@@ -0,0 +1,39 @@
+import React, { useReducer } from 'react';
+import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { initialState, reducer } from './reducers/reducer';
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  console.log(state)
+  const addTodo = (input) => {
+    const newTodo = {
+      todo: input,
+      completed: false,
+      id: 4
+    }
+    dispatch({type: "ADD_TODO", payload: newTodo})
+  }
+
+  const handleComplete = (id) => {
+    dispatch({type:"COMPLETED_TODO", payload: id})
+  }
+
+  const clearCompleted = () => {
+    dispatch({type: "CLEAR_COMPLETED_TODO"})
+  }
+
+  return (
+    <div className="App">
+      <h1>Ahmeds Todo</h1>
+      <TodoList state={state} handleComplete={handleComplete} />
+      <TodoForm addTodo={addTodo} />
+      <button onClick={(e) => {
+        e.preventDefault()
+        clearCompleted()
+      }}>Clear Task</button>
+    </div>
+  );
+}
+
+export default App;
