3774b617bc106aff019007dbe4de50ceca270320
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-04-09 00:55:31 -0400

    give stats full functionality

--- todo/src/components/Main/Stats/Stats.js
+++ todo/src/components/Main/Stats/Stats.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './scss/stats.scss';
 
 function Stats(props) {
+
+        //find the total people confirmed
+        const todoCompleted = props.state.todos.reduce((acc, current) => { return acc += current.completed },0);
+    
+        const todoToComplete = props.state.todos.length - todoCompleted;
+
     return (
     <div className="stats-box">
         <h1>Stats</h1>
         <p>Total todos: {props.state.todos.length}</p>
-        <p>Completed todos: </p>
-        <p>To be Completed todos: </p>
+        <p>Completed todos: {todoCompleted} </p>
+        <p>To be Completed todos: {todoToComplete} </p>
 
     </div>
     );
