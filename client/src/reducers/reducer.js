709452d129f8e0744abb347e349cd3a868e30541
Ricardo Barbosa <ric3639@gmail.com>
2020-04-08 18:44:52 -0700

    Step 1 complete

--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -1,5 +1,13 @@
+//export reducer
 export function reducer(state, action){
    default:
       return
 }
 
+//build initial state object that has a list of 
+//todos and export initial state object
+export const initialState = [{
+   item: 'Learned about reducers',
+   completed: false,
+   id: 12345678901112
+}]
