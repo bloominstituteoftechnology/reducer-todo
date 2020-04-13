3038c5c0d7f434aaf3fc3056f929cbce4fae8add
ph5500 <philfives@gmail.com>
2020-04-13 13:44:03 -0600

    fixed syntax errors in todo.js file

--- todo/src/components/todoComponents/Todo.js
+++ todo/src/components/todoComponents/Todo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-cons Todo = props => {
+const Todo = props => {
     return (
-        console.log("this is todo props", props);
-    <div className={`todo${props.todo.completed ? ' completed' : ''}`}
-        onClick={() => props.toggleTodo(props.todo.id)}>
-        <p className='todo'> {props.todo.task</p>
+        // console.log("this is todo props", props);
+        <div className={`todo${props.todo.completed ? ' completed' : ''}`}
+            onClick={() => props.toggleTodo(props.todo.id)}>
+            <p className='todo'> {props.todo.task}</p>
         </div>
-        )
+    )
 }
 
 export default Todo;
