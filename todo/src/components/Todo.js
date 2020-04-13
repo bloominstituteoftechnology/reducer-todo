095341bd3e418cb2f3f76336f28babb24c4ab24b
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-04-13 14:50:07 -0700

    Basic App is rendering

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let todo = () => {
+  return (
+    <div>
+      <p>This is where the tasks go</p>
+    </div>
+  )
+}
+
+export default todo;
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h2>Must complete:</h2>
+      <Todo />
+    </div>
+  )
+}
+
+export default TodoList;
