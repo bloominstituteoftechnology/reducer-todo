428a5b56190cf36723f5af422c16590277b7a718
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-13 15:17:31 -0500

    created reducer folder + file + initial state + simple reducer function with default return

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,17 @@
+import React, {useReducer} from 'react';
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+
+const reducer = (state,  action) => {
+
+
+
+ default: return state
+
+}
+
+export default reducer;
