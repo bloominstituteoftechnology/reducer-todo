d43ca4ad42c18f6f62dd7be014a0958b45801367
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-15 15:24:19 -0500

    have the cases going I think.

--- reducertodo/src/reducers/index.js
+++ reducertodo/src/reducers/index.js
@@ -1,13 +1,36 @@
-const initialState = [
-    {
-        item: 'learn useReducer',
-        id: 5619815445651654456,
-        completed: false,
-    },
-];
+const initialState = {
+    tasks: [
+        {
+            item: 'learn useReducer',
+            id: 5619815445651654456,
+            completed: false,
+        },
+        {
+            item: 'Complete useReducer challenge',
+            id: 5619815445651654459,
+            completed: false,
+        },
+    ],
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                tasks: [...state.tasks, action.payload],
+            };
+        case 'TOGGLE_TODO':
+            return {
+                ...state,
+                tasks: state.tasks.map((todo) => {
+                    if (todo.id === action.payload) {
+                        return {
+                            ...todo,
+                            completed: !todo.completed,
+                        };
+                    } else return todo;
+                }),
+            };
         default:
             return state;
     }
