0673fc0418b487a743ab175db98e4660e24eb780
John Rossi <DACOOK1983@gamil.com>
2020-05-16 13:39:33 -0400

    reduder and list files created and renders

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React, { useState, useReducer } from "react";
+
+// importing initialState and reducer
+// import { initialState, reducer } from "../reducers/todoReducer";
+
+
+const TodoList = () => {
+
+
+    return (
+        <>
+        <p>setup test</p>
+        </>
+    )
+}
+export default TodoList;
