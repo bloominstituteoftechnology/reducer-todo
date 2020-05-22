b342cdadce22e139435f43b7fc4ddac95744f843
Brian Clayton <finalb@projectfinalboss.com>
2020-05-21 21:11:47 -0400

    reducer complete

--- todo/src/reducer/index.js
+++ todo/src/reducer/index.js
@@ -1,7 +1,33 @@
+export const TOGGLE_COMPLETE = "TOGGLE_COMPLETE";
+
 export const initialTodoState = {
 
     item:"Learn about reducers",
     completed: false,
     id: 3892987589
+
+}
+
+
+export const todoReducer = (state, action) => {
+
+    switch(action.type) {
+
+        case TOGGLE_COMPLETE:
+            return {
+                ...state,
+                completed: !state.completed
+            };
+            case "END_ITEM_UPDATE":
+                return {
+                    ...state,
+                    item: action.payload ? action.payload: state.item,
+                    completed: false
+                };
+                default:
+                    return state;   
+
+    }
+
 
 }
