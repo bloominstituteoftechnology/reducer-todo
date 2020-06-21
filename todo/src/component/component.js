7e461e4ac2a923a3dc60ba8a3bf877531f9d5e37
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-06-21 18:58:45 -0400

    Added list with map, debugging

--- todo/src/component/component.js
+++ todo/src/component/component.js
@@ -1,10 +1,11 @@
 import React, { useState, useReducer } from "react";
-import { initialState, Reducer } from "../reducers";
+import { initialState, Reducer } from "../reducers/reducer";
 
 const Component = () => {
     const [newItem, setNewItem] = useState("");
 
     const [state, dispatch] = useReducer(Reducer, initialState);
+    console.log(state);
 
     const handleChanges = e => {
         setNewItem
@@ -13,7 +14,7 @@ const Component = () => {
 
     return (
         <div>
-            <h3>{state.item}{""} {!state.completed}</h3>
+            <h3>Reddit To Do List</h3>
 
             <input 
             className="item-name"
@@ -22,6 +23,21 @@ const Component = () => {
             value={newItem}
             onChange={handleChanges}
             />
+            <button onClick={() =>
+            dispatch({ type: "UPDATE_ITEM", payload: newItem })}>Add New Item</button>
+
+            <div>
+                <ul>
+                {state.map(itemMap => {
+                    return (<li onClick={() =>
+                        dispatch({ type: "UPDATE_COMPLETE" })}>{itemMap.item}</li>)
+                })}
+                    <li>{newItem}</li>
+                </ul>
+            </div>
+             
         </div>
     )
 }
+
+export default Component
