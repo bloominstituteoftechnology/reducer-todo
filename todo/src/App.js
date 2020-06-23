0c4b29745dd69835867f76bf4d09ce254b42b96a
Luke Adams <muhammadadams@gmail.com>
2020-06-23 15:26:19 -0400

    added component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,78 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { initialState, theReducer } from "./reducer";
 
 // you will need a place to store your state in this component.
 // design `App` to be the parent component of your application.
 // this component is going to take care of state, and any change handlers you need to work with your state
 
-const todoList = [
-  {
-    task: "Organize Garage",
-    id: 1528817077286,
-    completed: false,
-  },
-  {
-    task: "Bake Cookies",
-    id: 1528817084358,
-    completed: false,
-  },
-];
-
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor() {
-    super();
-    this.state = {
-      list: todoList,
-    };
-  }
-
-  addItem = (itemName) => {
+function App() {
+  const [state, dispatch] = useReducer(theReducer, initialState);
+  const addItem = (itemName) => {
     const newItem = {
       task: itemName,
       id: new Date(),
       completed: false,
     };
-    this.setState({
-      list: [...this.state.list, newItem],
-    });
+    dispatch({ type: "ADD_ITEM", payload: newItem });
   };
 
-  deleteItem = (e) => {
-    e.preventDefault();
-    this.setState({
-      list: this.state.list.filter((item) => !item.completed),
-    });
+  const deleteItem = (e) => {
+    dispatch({ type: "DELETE_TODO" });
   };
-  toggleItem = (itemId) => {
-    this.setState({
-      list: this.state.list.map((item) => {
-        if (item.id === itemId) {
-          return {
-            ...item,
-            completed: !item.completed,
-          };
-        } else {
-          return item;
-        }
-      }),
-    });
+
+  const toggleItem = (itemId) => {
+    dispatch({ type: "TOGGLE_ITEM", payload: itemId });
   };
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      <div className="App">
-        <TodoForm addItem={this.addItem} />
-        <TodoList
-          list={this.state.list}
-          toggleItem={this.toggleItem}
-          deleteItem={this.deleteItem}
-        />
-      </div>
-    );
-  }
-  };
+  return (
+    <div className="App">
+      <TodoForm addItem={addItem} />
+      <TodoList list={state} toggleItem={toggleItem} deleteItem={deleteItem} />
+    </div>
+  );
+}
+
+export default App;
--- todo/src/reducer.js
+++ todo/src/reducer.js
@@ -4,12 +4,16 @@
 
 // React redux- the reducer function will take in a state object (which is the current state) and an action object. Then it will create and return a brand new state object which will become the NEW current state.
 
-
 export const initialState = [
   {
-    item: "Learn about reducers",
+    task: "Organize Garage",
+    id: 1528817077286,
     completed: false,
-    id: 3892987589,
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false,
   },
 ];
 
@@ -19,4 +23,26 @@ export const initialState = [
 
 // Action must be objects, and they must have a "type:" property. They can alose have a payload which is (data) property as well.
 
- export function theReducer(state, action)
+export function theReducer(state, action) {
+  switch (action.type) {
+    //add item
+    case "ADD_ITEM":
+      return [...state, action.payload];
+    //toggle
+    case "TOGGLE_ITEM":
+      return state.map((item) => {
+        item.id === action.payload
+          ? {
+              ...item,
+              completed: !item.completed,
+            }
+          : item;
+      });
+
+    //delete
+    case "DELETE_ITEM":
+      return state.list.filter((item) => !item.completed);
+    default:
+      return state;
+  }
+}
