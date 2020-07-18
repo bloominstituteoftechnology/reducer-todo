03915fec6618950f5bc7f77fc477336653933fa0
theolamide <ajadidan@gmail.com>
2020-07-17 21:37:18 -0400

    More styling updates and font is poppingg!

--- reducer-todo/src/Components/TodoList.js
+++ reducer-todo/src/Components/TodoList.js
@@ -18,10 +18,11 @@ const TodoList = (props) => {
 
     return (
         <div className="listParent" >
-
             <p className="addedTask">
                 {props.state.map((element) => {
-                    return <Todo key={element.id} dispatch={props.dispatch} element={element} toggleComplete={element.completed} />
+                    if (element.id !== 3892987589) { //This conditional excludes the empty string of the initial state from being rendered
+                        return <Todo key={element.id} dispatch={props.dispatch} element={element} toggleComplete={element.completed} />
+                    }
                 })}
             </p>
             <button className="clearButton" onClick={filterCompleted}> Clear Done </button>
