18174b63975712fa0a9d163d60f237e02fcfda54
dustin <dustnclaytiner@gmail.com>
2020-07-22 15:17:11 -0500

    up and running

---
+++ todo/src/components/ToDoForm.js
@@ -0,0 +1,6 @@
+import react from "react"
+<label>
+    What to do?
+<input name='toDo'/>    
+</label>
+
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,22 @@
+
+export const initialState = [
+    {
+        item: 'Brush Teeth',
+        completed:false,
+        id:3928294839
+    },
+    {
+        item: 'Water Plants',
+        completed:false,
+        id:3928294838
+    },
+    {
+        item: 'Feed Cat',
+        completed:false,
+        id:3928294837
+    }
+]
+
+export const reducer = (state,action) => {
+return state;
+}
