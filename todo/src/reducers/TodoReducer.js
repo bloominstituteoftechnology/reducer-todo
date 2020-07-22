21d89b00908599846e506e77cbc77934cf1d8fee
JenVest2020 <jenvest2019@gmail.com>
2020-07-22 11:50:42 -0500

    cleaned up index&app/built reducer folder&file

---
+++ todo/src/reducers/TodoReducer.js
@@ -0,0 +1,12 @@
+
+export const initialState = {
+
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+};
+
+export const TodoReducer = (state, action) => {
+
+    return state;
+};
