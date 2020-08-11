ba8a1d98ae6ce1b5b8baeab5a5790a54ba773fab
timotheemcdonald <timotheemcdonald@gmail.com>
2020-08-11 10:46:25 -0400

    index js file for reducer working

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,20 +1,8 @@
-import React, {useState, useReducer} from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 import TodoList from './components/TodoList'
-import {reducer, initialState} from './reducers'
+import { reducer, initialState } from './reducers'
 
-// const defaultData = [
-//   {
-//     task: 'Finish Assignment',
-//     id: 1001,
-//     completed: false
-//   },
-//   {
-//     task: 'Watch Netflix',
-//     id: 1002,
-//     completed: false
-//   }
-// ]
 
 const blankTask = {
   task: ''
@@ -23,80 +11,51 @@ const blankTask = {
 function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState)
-  // const [todoData, setData] = useState(defaultData)
   const [taskText, setTask] = useState(blankTask)
 
-// const addTask = taskName => {
 
-  // console.log(todoData, 'tododata')
-  // console.log(newTask, 'new task')
-  // console.log(taskText, 'task text')
-
-  // setData(
-  //  [...todoData, newTask]
-  // )
-  
-
-
-
-const onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault()
-    // addTask(taskText)
-    dispatch({type: "ADD_TODO", payload: taskText.task})
+    dispatch({ type: "ADD_TODO", payload: taskText.task })
     setTask(blankTask)
   }
 
-const onChange = event => {
- const value = event.target.value
- setTask({...taskText, [event.target.name]:value})
+  const onChange = event => {
+    const value = event.target.value
+    setTask({ ...taskText, [event.target.name]: value })
   }
 
-const onComplete = event => {
-    // event.preventDefault()
-    // setData(
-    //   todoData.filter(task => {
-    //       return task.completed === false
-    //     })
-    // )
-    dispatch({type:"IS_CLEARED"})
-}
+  const onComplete = event => {
+    dispatch({ type: "IS_CLEARED" })
+  }
 
-const onToggle = id => {
-  // setData(
-  //    todoData.map(task => {
-  //     if (task.id === id){
-  //       return {...task, completed: !task.completed};
-  //     } else {
-  //       return task
-  //     }
-  //   })
-  // )
-  dispatch({type:"IS_COMPLETED", payload: state.id})
-}
+  const onToggle = id => {
+    dispatch({ type: "IS_COMPLETED", payload: state.id })
+  }
 
   return (
     <div className="App">
-    <h1>To Do List:</h1>
-    <form>
-            <div>
-                <input onSubmit={onSubmit}
-                type="text"
-                name="task"
-                value={taskText.task}
-                onChange={onChange}
-                />
-                <button onClick={onSubmit}>Add Task</button>
-            </div>
-            <div>
-                <button onClick={onComplete}>Clear Task</button>
-            </div>
-        </form>
+      <h1>To Do List:</h1>
+      <form>
+        <div>
+          <input onSubmit={onSubmit}
+            type="text"
+            name="task"
+            value={taskText.task}
+            onChange={onChange}
+          />
+          <button onClick={onSubmit}>Add Task</button>
+        </div>
+        <div>
+          <button onClick={onComplete}>Clear Task</button>
+        </div>
+      </form>
 
 
-    <TodoList task={initialState} onToggle={onToggle} dispatch={dispatch}/>
+      <TodoList task={initialState} onToggle={onToggle} dispatch={dispatch} />
     </div>
   );
-  
+
 }
 
 export default App;
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -10,37 +10,56 @@ export const initialState = [
 export const reducer = (state, action) => {
     switch (action.type) {
         case "ADD_TODO":
-            return
-            [...state,
+            return [...state,
             {
+                ...state,
                 task: action.payload,
                 id: new Date()
             }
             ]
 
         case "IS_COMPLETED":
-            return
-            [...state,
-            {
-                completed: state.task.completed.map(task => {
-                    if (task.id === action.payload)
-                        return [
-                            ...state,
-                            completed: !task.completed
-                        ]
-                    )
-            }
-            ]
+            return state.map(InitialState => {
+                if (state.id === action.payload) {
+                  return {...state, completed: !state.completed}
+                } else {
+                  return state
+                }
+              })
+            //   return
+            // [...state,
+            // {
+            //     completed: state.task.completed.map(task => {
+            //         if (task.id === action.payload)
+            //             return [
+            //                 ...state,
+            //                 {completed: !task.completed}
+            //             ]
+            //         }
+            //         )
+            // }
+            // ]
 
         case "IS_CLEARED":
-            return {
-                ...state,
-                task: state.task.filter(task => {
-                    return {
-                        task.completed === false
-                    }
-                })
-            }
+            return state.filter(task => 
+                task.completed === false)
+            
+            // state.filter (InitialState => {
+            //     return {...state, completed:
+            //     state.completed === false}
+            //     else {
+            //         return state
+            //     }
+            // })
+            // return {
+            //     ...state,
+            //     task: state.task.filter(task => {
+            //         return {
+            //             ...state,
+            //             task.completed === false
+            //         }
+            //     })
+            // }
         default:
             return state
 
