e14825792054ec1aa8c38a27e4cf9400b51c6597
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-08-19 20:30:40 -0400

    slowly making progress. returning name

--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,53 +1,42 @@
 import React, {useState, useReducer} from "react";
-import { listReducer, initialState } from "../reducers/indexReducer"
-
+//import { listReducer, initialState } from "../reducers/indexReducer"
+import { actions, initialState, listReducer, newToDo } from "../reducers/indexReducer.js";
 
 
 const Form = () =>{
-const [newText, setNewText] = useState("");
-const [state, dispatch] = useReducer(listReducer, initialState);
 
-    const handleChanges = (e) => {
-        setNewText(e.target.value);
-        console.log("going through handleChange")
-    };
+    const [todos, dispatch] = useReducer(listReducer, []);
+    const [newText, setNewText] = useState("");
+
+
+
+//const [state, dispatch] = useReducer(listReducer, initialState);
+
+    // const handleChanges = (e) => {
+    //     setNewText(e.target.value);
+    //     console.log("going through handleChange")
+    // };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        dispatch({type: actions.ADD_TODO, payload: {name: newText} });
+        setNewText("");
+        console.log("hey hey hanldeChange", {type: actions.ADD_TODO});
+    }
+
+    console.log(todos);
+
+  
 
     
 return(
+    <>
+        <form onSubmit = {handleSubmit}>
+            <input type= "text" value= {newText} onChange={e => 
+             setNewText(e.target.value)} />
+        </form>
 
-        <div> Edit Me!
-          {!state.completed ? (
-           <h3>
-            {state.item}{""}
-             <i 
-                onClick = {() => {
-                    dispatch({item: "TOGGLE_COMPLETED"});
-                }}
-                className = "list"
-            />
-           </h3>
-         ) : (
-             <div>
-             
-               <input className = "TODO"
-                type= "text"
-                name = "item"
-                value= {newText}
-                onChange = {handleChanges}
-                />
-                <button
-                    onClick={ ()=>{
-                        dispatch({type: "UPDATE_ITEM", payload: newText})
-                    }}
-                >
-                    Add to List
-                </button>
-              
-        
-    
-        </div>
-         )}
-    </div>
+     </>  
 
     );
 
@@ -56,3 +45,38 @@ return(
 
 
 export default Form;
+
+
+{/* <div> Edit Me!
+{state.completed ? (
+ <h3>
+  {state.item}{""}
+   <i 
+      onClick = {() => {
+          dispatch({item: "TOGGLE_COMPLETED"});
+      }}
+      className = "list"
+  />
+ </h3>
+) : (
+   <div>
+   
+     <input className = "TODO"
+      type= "text"
+      name = "item"
+      value= {newText}
+      onChange = {handleChanges}
+      />
+      <button
+          onClick={ ()=>{
+              dispatch({type: "UPDATE_ITEM", payload: newText})
+          }}
+      >
+          Add to List
+      </button>
+    
+
+
+</div>
+)}
+</div> */}
--- todo/src/reducers/indexReducer.js
+++ todo/src/reducers/indexReducer.js
@@ -5,17 +5,37 @@ export const initialState = {
     completed: false,
     id: 1
 
-}
-
-export const listReducer = (state, action) => {
-       switch(action.type) {
-         case "UPDATE_ITEM":
-            return {...state, item: action.payload, completed: false};
-         case "TOGGLE_COMPLETED":
-             return {...state, completed: !state.completed};  
-        default:
-            //console.log("Hello from the default in indexReducer", state);
-            return state;
-            
-    }
 };
+
+export const actions = {
+    ADD_TODO: "add-todo"
+};
+
+
+export function newToDo(name){
+     return  { id: Date.now(), name: name, complete: false}
+ }
+
+
+export const listReducer = (todos, action) => {
+    switch(action.type) {
+        case actions.ADD_TODO:
+            return [...todos, newToDo(action.payload.name)]
+    };
+
+};
+
+
+
+// export const listReducer = (state, action) => {
+//        switch(action.type) {
+//          case "UPDATE_ITEM":
+//             return {...state, item: action.payload, completed: false};
+//          case "TOGGLE_COMPLETED":
+//              return {...state, completed: !state.completed};  
+//         default:
+//             //console.log("Hello from the default in indexReducer", state);
+//             return state;
+            
+//     }
+// };
