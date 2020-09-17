8686e8f4cced5c9eec694e0e2c98f4094ee044c3
Tony Sorensen <tony48853@gmail.com>
2020-09-16 17:14:45 -0700

    Reducer file created setting initial state object. Reducer returning default state. State set in Todo component using reducer.

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./components/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">TaskMaster</header>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589,
+};
+
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
