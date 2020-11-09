824cda2bbe75096dcbf5f54f4c0cc7c674f7e49a
Katiuska <katiuskaalicea@gmail.com>
2020-11-08 21:26:13 -0800

    First Commit: Created reducer and actions directory. Installed react reduc and redux dependencies

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589,
+};
+
+export const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
