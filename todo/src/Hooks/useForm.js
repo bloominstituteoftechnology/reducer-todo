45677f21a88bd7a46087a870322ba0818bbeddda
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-11-11 15:45:16 -0700

    custom hook ready

---
+++ todo/src/Hooks/useForm.js
@@ -0,0 +1,15 @@
+// Making the custom hook
+import { useState } from 'react'
+
+export const useForm = initialValues => {
+  const [values, setTodo] = useState(initialValues)
+  const handleChange = e => {
+    setTodo(
+      {
+        todo: e.target.value
+      }
+    )
+  }
+
+  return [values, handleChange]
+}
