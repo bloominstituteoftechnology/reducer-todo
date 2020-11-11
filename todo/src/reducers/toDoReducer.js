469c0f3bf1fa5c6dd0d370c91a0fff884619e184
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-11-11 14:41:38 -0800

    Created my reducer and initial state

---
+++ todo/src/reducers/toDoReducer.js
@@ -0,0 +1,14 @@
+
+
+
+
+export const intitalState = {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      }
+
+
+export const toDoReducer = (state, action) => {
+    return (null)
+}
