5c9c4969208e3f14428928f7db6cdb35b7628596
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-11-12 11:52:50 -0800

    The start of the reducer

---
+++ todo/src/Reducers/todoReducer.js
@@ -0,0 +1,27 @@
+export const TOGGLE_DONE = "TOGGLE_DONE";
+export const SET_TODO = "SET_TODO";
+
+export const initTodoState = {
+  todo: "whatever you got to do on your busy schedule today :(!",
+  doneit: false
+};
+
+export const todoReducer = (state, action) => {
+  console.log(`NAL todoReducer.js: state,action: `, state, action);
+
+  switch (action.type) {
+    case TOGGLE_DONE:
+      return {
+        ...state,
+        doneit: !state.doneit
+      };
+    case SET_TODO:
+      return {
+        ...state,
+        todo: action.payload,
+        doneit: false
+      };
+    default:
+      return state;
+  }
+};
