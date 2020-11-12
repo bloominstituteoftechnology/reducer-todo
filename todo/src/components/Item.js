b27e863c2829ec0fb11c8dc0726fdc85791716f2
Katiuska <katiuskaalicea@gmail.com>
2020-11-11 21:51:50 -0800

    passed down props to the iyem component, but drilling

--- todo/src/components/Item.js
+++ todo/src/components/Item.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Item = (props) => {
+  console.log("Props from inside the Item component: ", props);
   return (
     <div>
       <h3>Item.js</h3>
-      {props.task.map((item) => (
+
+      {/* {props.task.map((item) => (
         <div key={item.id}>
           <p>{item.item}</p>
           {item.completed === false ? <p>Completed</p> : <></>}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-import { holdTodo } from "../actions";
 
 import Item from "./Item";
 
 const List = (props) => {
-  console.log("Props passed down to the List component: ", props);
+  console.log("Props passed down to the List component: ", props.tasks);
   return (
     <div>
       <h3>This is the List component. </h3>
+      {props.tasks.map((task) => (
+        <Item tasks={task} key={task.id} />
+      ))}
     </div>
   );
 };
