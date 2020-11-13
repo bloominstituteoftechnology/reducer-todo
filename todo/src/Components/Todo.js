08fb8092cc1b118f9531731b3463345810008db1
kato hawkins <kristhahawkins@icloud.com>
2020-11-12 21:29:42 -0500

    MVP

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,22 +1,15 @@
-
 import React from "react";
 
-const Todo =(props) =>{
-  
+const Todo = (props) =>{
     return(
-        <div 
-        onClick={() => props.toggleTask(props.todo.id)}
-         className={`task${props.todo.completed ?" completed" : ""}`}
-        >
-          
-        <p>{props.todo.item}</p>
-        {console.log("props",props)}
-         
-        </div>
+        <div
+        onClick={() => props.toggleTask(props.todo.id)}//setting up the onClick to toggle the task from not completed to completed
+        //then we are putting those completed task into their own class so we can grey the task out
+        className= {`task${props.todo.completed ?"completed" : ""}`} > 
         
-    );
-};
-
+        <p>{props.todo.item}</p> 
+        {console.log("props",props)}
+        </div>//adding the task to the page
+    )
+}
 export default Todo;
-
-
