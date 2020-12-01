cf9f8cdd5927af615b2fa346806bded6949e6138
Ryan Clark <rbclark244@gmail.com>
2020-11-30 22:02:34 -0500

    MVP

--- todo/src/Components/TodoForm.js
+++ todo/src/Components/TodoForm.js
@@ -1,72 +1,72 @@
-// import React, { useState } from "react";
-// // import useTodoForm from "../hooks/useTodoForm";
+import React, { useState } from "react";
+// import useTodoForm from "../hooks/useTodoForm";
 
-// const TodoForm = (props) => {
-//   const [item, setItem] = useState();
-//   //     {
-//   //   item: "",
-//   //   id: Date.now(),
-//   //   completed: false,
-//   // }
-//   //   const [values, handleChanges, initialValue] = useTodoForm(initialValue);
+const TodoForm = (props) => {
+  const [item, setItem] = useState();
+  //     {
+  //   item: "",
+  //   id: Date.now(),
+  //   completed: false,
+  // }
+  //   const [values, handleChanges, initialValue] = useTodoForm(initialValue);
 
-//   const handleChanges = (e) => {
-//     setItem({ ...item, [e.target.name]: e.target.value });
-//   };
-
-//   const submitItem = (e) => {
-//     e.preventDefault();
-//     setItem({ item: "", id: Date.now(), completed: false });
-//     props.addItem(e, item.item);
-//   };
-
-//   console.log(item);
-
-//   return (
-//     <div>
-//       <form onSubmit={submitItem}>
-//         <input type="text" value={item} name="item" onChange={handleChanges} />
-//         <button>Add Item</button>
-//       </form>
-//     </div>
-//   );
-// };
-// export default TodoForm;
-
-import React from "react";
-
-class TodoForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      item: "",
-    };
-  }
-
-  handleChanges = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChanges = (e) => {
+    setItem({ ...item, [e.target.name]: e.target.value });
   };
 
-  submitItem = (e) => {
+  const submitItem = (e) => {
     e.preventDefault();
-    this.setState({ item: "", id: Date.now(), completed: false });
-    this.props.addItem(e, this.state.item);
+    setItem({ item: "", id: Date.now(), completed: false });
+    props.addItem(e, item.item);
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.submitItem}>
-          <input
-            type="text"
-            value={this.state.item}
-            name="item"
-            onChange={this.handleChanges}
-          />
-          <button>Add Item</button>
-        </form>
-      </div>
-    );
-  }
-}
+  console.log(item);
+
+  return (
+    <div>
+      <form onSubmit={submitItem}>
+        <input type="text" value={item} name="item" onChange={handleChanges} />
+        <button>Add Item</button>
+      </form>
+    </div>
+  );
+};
 export default TodoForm;
+
+// import React from "react";
+
+// class TodoForm extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       item: "",
+//     };
+//   }
+
+//   handleChanges = (e) => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   };
+
+//   submitItem = (e) => {
+//     e.preventDefault();
+//     this.setState({ item: "", id: Date.now(), completed: false });
+//     this.props.addItem(e, this.state.item);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.submitItem}>
+//           <input
+//             type="text"
+//             value={this.state.item}
+//             name="item"
+//             onChange={this.handleChanges}
+//           />
+//           <button>Add Item</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+// export default TodoForm;
