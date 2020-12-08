acdbc65ffb1e6aec7539a65819094b427b22be3e
duointherain <duointherain@gmail.com>
2020-12-07 23:42:20 -0800

    added images

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,6 +6,9 @@ import reducer from './reducers';
 import { setNewTodo, setCompleted, setCurrentText, setClear } from './actions/index';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import image1 from './images/pir1.png';
+import image2 from './images/pir2.png';
+
 
 const App = () => {
 
@@ -50,8 +53,16 @@ const App = () => {
 
   return(
     <div className='App'>
+      <div className='image1'>
+      <img src={image1} alt="smiley face pirate with an eyepatch" />;
+      </div>
+      <div>
       <TodoForm handleAddItem={handleAddItem} state={state} handleChanges={handleChanges} handleClearTasks={handleClearTasks}/>
       <TodoList todoList={state.list} handleToggleItem={handleToggleItem}/>
+      </div>
+      <div className='image2'>
+      <img src={image2} alt="red pirate wax seal flourish" />;
+      </div>
     </div>
 
 )}
