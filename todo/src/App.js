de93cbfe4c53ba606892d8a6019ed1dd08e452d8
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-12-16 19:55:11 -0500

    started adding functionality.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,22 @@
 import React, { useState, useReducer } from 'react';
 import './App.css';
 import { todoReducer } from './Reducers/todoReducer';
+import { initialState } from './Reducers/todoReducer';
 
 function App() {
 
-  const [state, dispatch] = useReducer(todoReducer)
-  
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [newItem, setNewItem] = useState('');
+
+  const handleChanges = (e) => {
+    setNewItem(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>My To Do List</h1>
+        {state.item}
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,6 +29,18 @@ function App() {
         >
           Learn React
         </a> */}
+        <div>
+          <input
+            type='text'
+            name="todoItem"
+            value={newItem}
+            onChange={handleChanges}
+          />
+          <button className='additem'>Add Todo Item</button>
+          <div>
+            <button className='clearitems'>Clear Completed</button>
+          </div>
+        </div>
       </header>
     </div>
   );
