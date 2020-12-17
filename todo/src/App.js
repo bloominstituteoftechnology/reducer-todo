90fd018d9a3ace492f45c92f33de61fee7cc402c
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-12-17 09:35:57 -0500

    added actions: ADD_TODO & TOGGLE_TODO

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+import todoReducer from './reducers/reducer';
+// import initialState from './reducers/reducer';
+
+const initialState = {
+	todos: [
+		{
+			item: 'Learn reducers',
+			completed: false,
+			id: Date.now()
+		}
+	]
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const [todos, setTodos] = useState({});
+	const [newTodo, setNewTodo] = useState('');
+	const [state, dispatch] = useReducer(todoReducer, initialState);
+
+	const changeHandler = e => {
+		setNewTodo(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		dispatch({ type: 'ADD_TODO', payload: newTodo });
+		setNewTodo('');
+	};
+
+	console.log(state.todos);
+
+	return (
+		<div className="App">
+			<h1>Todo App</h1>
+			<form onSubmit={handleSubmit}>
+				<input className="todo__input" type="text" value={newTodo} onChange={changeHandler} />
+				<button disabled={!newTodo} type="submit">
+					ADD
+				</button>
+			</form>
+			<div>
+				<TodoList todos={state.todos} dispatch={dispatch} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
