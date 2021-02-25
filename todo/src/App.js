64f2bc86fecef0259f2af0922731920470677047
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-25 02:59:33 -1000

    added gsap animation on state.length changetocards

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import DehazeIcon from "@material-ui/icons/Dehaze";
 import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
-import TodoForm from './components/TodoForm';
+import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-
   return (
-    <div className="App"> 
-       <AppBar position="sticky" style={{boxShadow: '0 0 1.5rem black'}}>
-          <Toolbar color='inherit'>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <DehazeIcon />
-            </IconButton>
-            <h1>Pop Todo!</h1>
-          </Toolbar>
-        </AppBar>
+    <div className="App">
+      <AppBar
+        className="app-bar"
+        position="sticky"
+        style={{
+          boxShadow: "0 0 1.5rem black",
+          backgroundColor: 'orangered',
+          color: "white",
+          opacity: '0.9'
+        }}
+      >
+        <Toolbar color="inherit">
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <DehazeIcon />
+          </IconButton>
+          <h1>Pop Todo!</h1>
+        </Toolbar>
+      </AppBar>
       <TodoForm />
     </div>
   );
 }
 
 export default App;
-
-
--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useRef, useEffect, useState, useReducer } from "react";
 import { todoReducer, initialState } from "../reducers/todoSwitchReducer";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -6,30 +6,69 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import TextField from '@material-ui/core/TextField';
-import MaskUp from '../assets/maskup.jpg'
+import TextField from "@material-ui/core/TextField";
+import { TweenMax, Power3 } from "gsap";
+import MaskUp from "../assets/maskup.jpg";
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 550,
-    maxWidth: 650,
-    minHeight: 475,
+    minWidth: "40%",
+    maxWidth: "45%",
+    minHeight: "42.5%",
     borderRadius: "50%",
-    margin: "7rem 5%",
+    margin: "7rem 2.5%",
     textAlign: "center",
     backgroundImage: `url(${MaskUp})`,
-    backgroundPosition: 'center center',
-    backgroundSize: '100%',
-    backgroundRepeat: 'no-repeat',
-    boxShadow: '0 0 3rem orangered',
-    transition: 'boxShadow 100ms ease-out'
-
+    backgroundPosition: "left bottom",
+    backgroundSize: "50%",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    boxShadow: "0 0 3rem orangered",
   },
   title: {
     fontSize: 14,
   },
   pos: {
     marginBottom: 12,
+  },
+  nav: {
+    color: "black",
+    margin: "2rem 0",
+    backgroundColor: "#f8d568",
+    padding: "1.5rem",
+  },
+  addButton: {
+    margin: "4rem 2rem",
+  },
+  cardContent: {
+    backgroundColor: "#222",
+    opacity: "0.8",
+  },
+  h2: {
+    margin: "6rem 2rem",
+    marginBottom: "1rem",
+    color: "white",
+    textShadow: "0 0 1.5rem orangered",
+  },
+  h1: {
+    fontSize: "3rem",
+    padding: "0 5rem",
+    color: "white",
+    textShadow: "0 0 1.5rem orangered",
+  },
+  popButton: {
+    backgroundColor: "red",
+    color: "white",
+    marginBottom: "6rem",
+    maxWidth: "35%",
+    maxHeight: "20%",
+    boxShadow: "0 0 1.5rem orangered",
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#222",
+    opacity: "0.8",
   },
 });
 
@@ -38,6 +77,7 @@ export default function TodoForm() {
 
   const [newTodo, setNewTodo] = useState("");
   const [state, dispatch] = useReducer(todoReducer, initialState);
+  let card = useRef(null)
 
   const handleChanges = (e) => {
     setNewTodo(e.target.value);
@@ -45,7 +85,7 @@ export default function TodoForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: "ADD_TODO", payload: newTodo })
+    dispatch({ type: "ADD_TODO", payload: newTodo });
     setNewTodo("");
   };
 
@@ -55,39 +95,67 @@ export default function TodoForm() {
     }
   };
 
+  const animatedState = state.length;
+
+  
+  useEffect(() => {
+    if (animatedState !== 0 ){
+   
+    TweenMax.from(
+      card, //item
+      3, // duration
+      {
+        //gsap options
+        opacity: 0,
+        y: 200,
+        ease: Power3.easeInOut,
+      }
+    );}
+  }, [animatedState]);
+  
   return (
-    <div style={{paddingBottom: '4rem'}}>
+    <div style={{ paddingBottom: "4rem" }}>
       <form position="sticky" className="bg-dark" onSubmit={handleSubmit}>
         <TextField
+          className={classes.nav}
           name="newTodo"
           id="newTodo"
           type="text"
           onChange={handleChanges}
-          color='inherit'
+          color="inherit"
           value={newTodo}
           placeholder="Enter Todo"
-          helperText='Click your task to mark as done then hit the button and POP!!'
+          helperText="Click your task to mark as done then hit the button and POP!!"
           onSubmit={handleSubmit}
-          style={{color: 'white', margin: '2rem 0', backgroundColor: 'white', padding: '1.5rem'}}
         />
-        <Button variant='outlined' size='large' color='primary' style={{margin: '4rem 2rem'}}
+        <Button
+          className={classes.addButton}
+          variant="outlined"
+          size="large"
+          color="primary"
+          style={{}}
           onClick={() => dispatch({ type: "ADD_TODO", payload: newTodo })}
         >
           Add Todo
         </Button>
-       
       </form>
 
       {state.map((item) => {
         return (
-          <Card className={classes.root} variant="outlined">
-            <CardContent style={{backgroundColor: '#222', opacity: '0.8'}}>
+          <Card
+            className={classes.root}
+            variant="outlined"
+            ref={(element) => {
+              card = element;
+            }}
+          >
+            <CardContent className={classes.cardContent}>
               <Typography
                 className={classes.title}
                 color="textSecondary"
                 gutterBottom
               >
-                <h2 style={{ margin: "6rem 2rem", marginBottom: '1rem', color: 'white', textShadow: '0 0 1.5rem orangered' }}>I feel like I need to...</h2>
+                <h2 className={classes.h2}>I feel like I need to...</h2>
               </Typography>
               <Typography
                 variant="body2"
@@ -98,19 +166,17 @@ export default function TodoForm() {
                   dispatch({ type: "TOGGLE_EDITING", payload: item.id })
                 }
               >
-                <h1 style={{ fontSize: "3rem", padding: "0 5rem", color: 'white', textShadow: '0 0 1.5rem orangered' }}>
-                  {item.task}!!
-                </h1>
+                <h1 className={classes.h1}>{item.task}!!</h1>
               </Typography>
             </CardContent>
-            <CardActions className="d-flex justify-content-center" style={{backgroundColor: '#222', opacity: '0.8'}}>
+            <CardActions className={classes.cardActions}>
               <Button
                 variant="contained"
                 size="small"
-                style={{backgroundColor: 'red', color: 'white', marginBottom: '6rem', maxWidth: '35%', maxHeight: '20%', boxShadow: '0 0 1.5rem orangered'}}
+                className={classes.popButton}
                 onClick={() => dispatch({ type: "CLEAR_COMPLETED" })}
               >
-                <h5 style={{paddingBottom: '1rem'}}>HURRY! Pop me!!</h5>
+                <h5 style={{ paddingBottom: "1rem" }}>HURRY! Pop me!!</h5>
               </Button>
             </CardActions>
           </Card>
