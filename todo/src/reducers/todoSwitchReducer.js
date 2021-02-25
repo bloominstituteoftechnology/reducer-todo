65f44ad6e2e317c85e30863d87bd6cf92f89d571
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-02-25 11:42:02 -1000

    psuedo code added for later forensics

--- todo/src/reducers/todoSwitchReducer.js
+++ todo/src/reducers/todoSwitchReducer.js
@@ -1,3 +1,7 @@
+//useReducer takes in two arguments state and action. setting up a reducer 
+//involves initializing state and setting up a switch statement that handles all expected cases of state change
+
+//Initial State
 export const initialState = [
   {
     task: 'Keep Calm.. and POP',
@@ -6,9 +10,12 @@ export const initialState = [
   }
 ];
 
+//reducer taking in state and action from useReducer
 export const todoReducer = (state, action) => {
-
+  //Switch statement takes in action.type ('ADD_TODO, 'TOGGLE_EDITING', 'CLEAR_COMPLETED') as cases and returns one 
+  //three processes
   switch(action.type) {
+      //ADD a new todo by taking state, making a shallow copy and appending a new item to state using same format as initial state
       case('ADD_TODO'):
           return [...state,
               {
@@ -16,6 +23,8 @@ export const todoReducer = (state, action) => {
                   id: Date.now(),
                   completed: false
               }]
+      //TOGGLE editing maps through state and if the todo.id === action.payload then create a shallow copy of the item and complete: not item.completed. 
+      //otherwise return the item
       case('TOGGLE_EDITING'):
           return state.map(item => {
               if(item.id === action.payload) {
@@ -27,6 +36,7 @@ export const todoReducer = (state, action) => {
                   return item
               }
           })
+      //CLEAR filters through state and takes the item and makes it go by bye. 
       case('CLEAR_COMPLETED'):
           return state.filter(item => !item.completed)
       default:
