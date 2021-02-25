b246e1492f0bd6493530421da183a5e53a85276e
miketillman95 <matillman95@gmail.com>
2021-02-24 20:42:32 -0800

    hmwk

---
+++ todo/src/Todo.js
@@ -0,0 +1,10 @@
+import { useReducer } from "react";
+
+
+const Todo = () =>{
+
+  const initialState = { title: 'Todo List', editing: true}
+  const [state, dispatch] = useReducer(titlereduc)
+}
+
+export default Todo;
