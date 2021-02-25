8f816ffb4c048e4f68c5380d3361d25dd406b1cf
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-02-24 19:07:11 -0700

    included handle click on <li> where on click the added todo will be deleted!

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -7,12 +7,16 @@ export default function Todo() {
 
 const Todos = () => {
 const todos = useSelector(state => state.todos);
+const handleClick = id => dispatch({
+    type: 'DELETE_TODO',
+    payload: id,
+})
 if (!todos || !todos.length) {
     return <p>Relax... you have nothing on your list!</p>
 }
 return (
     <ul>
-    {todos.map(todo => <li>{todo.label}</li>)}
+    {todos.map(todo => <li onClick={() => handleClick(todo.id)}>{todo.label}</li>)}
     </ul>
 )
 }
