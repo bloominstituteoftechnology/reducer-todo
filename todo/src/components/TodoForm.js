6acab22ff5f17f2cc3db56e4ec0aa7f979881972
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-10-18 23:10:21 -1000

    Update TodoForm.js

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -56,7 +56,7 @@ export default function TodoForm() {
 
   return (
     <div style={{ paddingBottom: "6rem" }}>
-      <form position="sticky" className="bg-dark d-flex align-items-center justify-items-center justify-content-center" onSubmit={handleSubmit}>
+      <form position="sticky" className="bg-dark d-flex flex-column flex-wrap align-items-center justify-items-center justify-content-center" onSubmit={handleSubmit}>
         <TextField
           className={classes.nav}
           name="newTodo"
